6eaf90b3a16a4d7bdc19c5edf3ca3bcf5a716c3d
Tran Tuan Hiep <honza.tran@gmail.com>
2017-08-14 23:30:50 +0200

    Add FindPCMWrapper.cmake

---
+++ FindPCMWrapper.cmake
@@ -0,0 +1,8 @@
+
+set(PCM_WRAPPER_INCLUDE_DIRS  "${CMAKE_CURRENT_LIST_DIR}/../include")
+
+set (PCM_WRAPPER_DEFINITIONS "-std=c++14")
+
+file(GLOB PCM_WRAPPER_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/../lib/*.dylib" 
+                                "${CMAKE_CURRENT_LIST_DIR}/../lib/*.a"
+                                "${CMAKE_CURRENT_LIST_DIR}/../lib/*.so")
