45f8b4ab39127913ccc17b8a9ee016b271849eb1
Tran Tuan Hiep <honza.tran@gmail.com>
2017-08-15 22:53:25 +0200

    Find Pcm wrapper downgrade definitions to c++11

--- FindPCMWrapper.cmake
+++ FindPCMWrapper.cmake
@@ -1,7 +1,7 @@
 
 set(PCM_WRAPPER_INCLUDE_DIRS  "${CMAKE_CURRENT_LIST_DIR}/../include")
 
-set (PCM_WRAPPER_DEFINITIONS "-std=c++14")
+set (PCM_WRAPPER_DEFINITIONS "-std=c++11")
 
 file(GLOB PCM_WRAPPER_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/../lib/*.dylib" 
                                 "${CMAKE_CURRENT_LIST_DIR}/../lib/*.a"
