576a371320bf9c5040dc74d2abc322013dde6590
Tran Tuan Hiep <honza.tran@gmail.com>
2017-08-15 22:51:32 +0200

    moving json to a 3rd party directory  + changing the cmake list

---
+++ 3rd-party/cmake/FindJSON.cmake
@@ -0,0 +1,4 @@
+
+set(JSON_INCLUDE_DIRS  "${CMAKE_CURRENT_LIST_DIR}/../json/include")
+
+set (SON_DEFINITIONS "-std=c++11")
--- json/cmake/FindJSON.cmake
+++
@@ -1,4 +0,0 @@
-
-set(JSON_INCLUDE_DIRS  "${CMAKE_CURRENT_LIST_DIR}/../include")
-
-set (SON_DEFINITIONS "-std=c++11")
