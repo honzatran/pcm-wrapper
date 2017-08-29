03628944c81a46bbb85403196f253312e715c037
Tran Tuan Hiep <honza.tran@gmail.com>
2017-08-29 23:53:36 +0200

    added pcm cleanum on fatal error

--- error_handling.cpp
+++ error_handling.cpp
@@ -3,6 +3,8 @@
 #include "error_handling.hpp"
 
 #include <iostream>
+#include <vector>
+#include <pcm/cpucounters.h>
 
 using namespace std;
 
@@ -13,6 +15,8 @@ void defaultFatalHandler(char const* errorMsg) {
 
 
 std::function<void(char const*)> g_fatalErrorHandler = defaultFatalHandler;
+
+std::vector<std::function<void()>> g_onFatalErrors;
 
 void
 detailedDefaultFatalHandler(char const* errorMsg,
@@ -25,14 +29,22 @@ detailedDefaultFatalHandler(char const* errorMsg,
 std::function<void(char const*, char const*, int const)>
     g_detailedFatalErrorHandler = detailedDefaultFatalHandler;
 
+void
+runOnFatalError() {
+    auto *const pcm = PCM::getInstance();
+    if (pcm != nullptr) {
+        pcm->cleanup();
+    }
+}
+
 void handleFatalError(char const* errorMsg) {
+    runOnFatalError();
     g_fatalErrorHandler(errorMsg);
 }
 
 void
 handleFatalError(char const* errorMsg, char const* file, int const line) {
+    runOnFatalError();
     g_detailedFatalErrorHandler(errorMsg, file, line);
 }
-
-
 
